c75889ab4f8af9ce7f21da4b16f079cc041ec6f0
Ubuntu <ubuntu@ip-172-31-22-222.us-east-2.compute.internal>
2021-10-18 01:09:16 +0000

    adds sources, tests,  descriptions

--- models/customers.sql
+++ models/customers.sql
@@ -5,7 +5,7 @@ with customer_orders as (
 select customer_id
       ,min(created_at) as first_order_at
       ,count(distinct id) as number_of_orders
-from `analytics-engineers-club.coffee_shop.orders` as orders
+from {{ source('coffee_shop', 'orders') }} as orders
 group by 1
 
 )
@@ -16,5 +16,5 @@ select customer_orders.customer_id
       ,customer_orders.first_order_at
       ,customer_orders.number_of_orders
 from customer_orders
-left join `analytics-engineers-club.coffee_shop.customers` as customers on customers.id = customer_orders.customer_id
+left join {{ source('coffee_shop', 'customers') }} as customers on customers.id = customer_orders.customer_id
 order by first_order_at 
